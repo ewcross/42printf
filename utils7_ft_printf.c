37641d72e2293e8f482a317d33a0c7ae6bcc64c8
Elliot CROSS <ecross@e3r9p18.42.fr>
2019-12-04 18:10:40 +0100

    snakes

--- utils7_ft_printf.c
+++ utils7_ft_printf.c
@@ -1,36 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils7_ft_printf.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ecross <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/22 16:05:07 by ecross            #+#    #+#             */
-/*   Updated: 2019/12/04 17:42:12 by ecross           ###   ########.fr       */
+/*   Created: 2019/12/04 17:28:49 by ecross            #+#    #+#             */
+/*   Updated: 2019/12/04 17:43:47 by ecross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-#include <stdlib.h>
-#include <locale.h>
 
-/*types: c s p di u x X f e n*/
-/*
-test ' flag with:
-			- #
-			- precision alone
-			- precision with 0 flag
-*/
-
-int main(int argc, char **v)
+int		float_specials(double arg, char **var_addr)
 {
-	(void)argc;
-	(void)v;
-	
-	setlocale(LC_NUMERIC, "en_GB");
+	int n;
 
-	ft_printf("mine: ...%06.6f...\n", INFINITY);
-	printf("real: ...%06.6f...\n", INFINITY);
-	ft_printf("mine: ...%f...\n", NAN);
-	printf("real: ...%f...\n", NAN);
+	n = NAN;
+	if (arg == INFINITY)
+	{
+		*var_addr = ft_strdup("inf");
+		return (1);
+	}
+	else if (arg == -INFINITY)
+	{
+		*var_addr = ft_strdup("-inf");
+		return (1);
+	}
+	else
+		return (0);
 }
+
